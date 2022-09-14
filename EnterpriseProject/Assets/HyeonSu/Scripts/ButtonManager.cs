7d7ca07336c66f38065081c5b8628b2c04d83f09
qkrgustn <sdh210212@sdh.hs.kr>
2022-09-14 09:46:57 +0900

    창 나오는 연출

--- EnterpriseProject/Assets/HyeonSu/Scripts/ButtonManager.cs
+++ EnterpriseProject/Assets/HyeonSu/Scripts/ButtonManager.cs
@@ -2,16 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 public class ButtonManager : MonoBehaviour
 {
+    Vector2 OnPos = new Vector2(0, 0), OffPos = new Vector2(0, -3000);
+
+    [SerializeField] private GameObject TouchWindow;
+    
+    public void Facility()
+    {
+        OpenProduction(TouchWindow);    
+    }
     public void GoToDormitory()
     {
 
     }
 
-    //창 열리는 연출
-    void DirectingOut(GameObject Window)
+    public void Exit()
     {
-
+        CloseProduction(EventSystem.current.currentSelectedGameObject.transform.parent.gameObject);
+    }
+    //창 열리는 연출
+    void OpenProduction(GameObject Window)
+    {
+        Window.transform.DOLocalMove(OnPos, 0.75f);
+    }
+    //창 닫히는 연출
+    void CloseProduction(GameObject Window)
+    {
+        Window.transform.DOLocalMove(OffPos, 0.75f);
     }
 }
