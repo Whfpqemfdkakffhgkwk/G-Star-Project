97e3d9fee99ca0c5ecd9ca53b41d7b9147ffea3f
qkrgustn <sdh210212@sdh.hs.kr>
2022-09-14 09:06:59 +0900

    닷트윈 추가

--- EnterpriseProject/Assets/HyeonSu/Scripts/ButtonManager.cs
+++ EnterpriseProject/Assets/HyeonSu/Scripts/ButtonManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class ButtonManager : MonoBehaviour
 {
-    void Facility()
+    public void GoToDormitory()
+    {
+
+    }
+
+    //창 열리는 연출
+    void DirectingOut(GameObject Window)
     {
 
     }
