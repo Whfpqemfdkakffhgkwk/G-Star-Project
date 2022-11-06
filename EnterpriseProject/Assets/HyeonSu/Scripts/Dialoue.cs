1c5448a9b0626dc805bc628402c9e4b57df74845
qkrgustn <sdh210212@sdh.hs.kr>
2022-11-06 21:02:33 +0900

    일단 다이얼로그 코드 손봐야할듯

--- EnterpriseProject/Assets/HyeonSu/Scripts/Dialoue.cs
+++ EnterpriseProject/Assets/HyeonSu/Scripts/Dialoue.cs
@@ -17,7 +17,32 @@ public struct TalkData
 [Serializable]
 public struct TalkDatas
 {
-    public List<TalkData> talks;
+    //깨끗하게 정리할 것.
+    public List<TalkData> LeeTaeyeon1;
+    public List<TalkData> LeeTaeyeon2;
+    public List<TalkData> LeeTaeyeon3;
+    public List<TalkData> LeeTaeyeon4;
+    public List<TalkData> LeeTaeyeon5;
+    public List<TalkData> JeongSeoYoon1;
+    public List<TalkData> JeongSeoYoon2;
+    public List<TalkData> JeongSeoYoon3;
+    public List<TalkData> JeongSeoYoon4;
+    public List<TalkData> JeongSeoYoon5;
+    public List<TalkData> LeeYerin1;
+    public List<TalkData> LeeYerin2;
+    public List<TalkData> LeeYerin3;
+    public List<TalkData> LeeYerin4;
+    public List<TalkData> LeeYerin5;
+    public List<TalkData> SongYeonHa1;
+    public List<TalkData> SongYeonHa2;
+    public List<TalkData> SongYeonHa3;
+    public List<TalkData> SongYeonHa4;
+    public List<TalkData> SongYeonHa5;
+    public List<TalkData> SeongJunAh1;
+    public List<TalkData> SeongJunAh2;
+    public List<TalkData> SeongJunAh3;
+    public List<TalkData> SeongJunAh4;
+    public List<TalkData> SeongJunAh5;
 }
 public class Dialoue : MonoBehaviour
 {
@@ -26,25 +51,57 @@ public class Dialoue : MonoBehaviour
     [SerializeField] private Image Background;
     [SerializeField] private Sprite[] BKspr;
     [SerializeField] private Image[] Character;
+    Dictionary<string, List<TalkData>> TalkDic = new Dictionary<string, List<TalkData>>();
 
     private void Start()
     {
-        StartCoroutine(StoryStart());
-
-    }
-    IEnumerator StoryStart()
-    {
-        var loadedJson = Resources.Load<TextAsset>("AAA");
+        var loadedJson = Resources.Load<TextAsset>("TalkData");
         var StoryData = JsonUtility.FromJson<TalkDatas>(loadedJson.ToString());
-        for (int i = 0; i < StoryData.talks.Count; i++)
+        #region 대화묶음들 가져오기
+        TalkDic.Add("LeeTaeyeon1", StoryData.LeeTaeyeon1);
+        TalkDic.Add("LeeTaeyeon2", StoryData.LeeTaeyeon2);
+        TalkDic.Add("LeeTaeyeon3", StoryData.LeeTaeyeon3);
+        TalkDic.Add("LeeTaeyeon4", StoryData.LeeTaeyeon4);
+        TalkDic.Add("LeeTaeyeon5", StoryData.LeeTaeyeon5);
+
+        TalkDic.Add("JeongSeoYoon1", StoryData.JeongSeoYoon1);
+        TalkDic.Add("JeongSeoYoon2", StoryData.JeongSeoYoon2);
+        TalkDic.Add("JeongSeoYoon3", StoryData.JeongSeoYoon3);
+        TalkDic.Add("JeongSeoYoon4", StoryData.JeongSeoYoon4);
+        TalkDic.Add("JeongSeoYoon5", StoryData.JeongSeoYoon5);
+        
+        TalkDic.Add("LeeYerin1", StoryData.LeeYerin1);
+        TalkDic.Add("LeeYerin2", StoryData.LeeYerin2);
+        TalkDic.Add("LeeYerin3", StoryData.LeeYerin3);
+        TalkDic.Add("LeeYerin4", StoryData.LeeYerin4);
+        TalkDic.Add("LeeYerin5", StoryData.LeeYerin5);
+
+        TalkDic.Add("SongYeonHa1", StoryData.SongYeonHa1);
+        TalkDic.Add("SongYeonHa2", StoryData.SongYeonHa2);
+        TalkDic.Add("SongYeonHa3", StoryData.SongYeonHa3);
+        TalkDic.Add("SongYeonHa4", StoryData.SongYeonHa4);
+        TalkDic.Add("SongYeonHa5", StoryData.SongYeonHa5);
+
+        TalkDic.Add("SeongJunAh1", StoryData.SeongJunAh1);
+        TalkDic.Add("SeongJunAh2", StoryData.SeongJunAh2);
+        TalkDic.Add("SeongJunAh3", StoryData.SeongJunAh3);
+        TalkDic.Add("SeongJunAh4", StoryData.SeongJunAh4);
+        TalkDic.Add("SeongJunAh5", StoryData.SeongJunAh5);
+        #endregion
+        StartCoroutine(StoryStart("JeongSeoYoon5"));
+    }
+    IEnumerator StoryStart(string TalkSelect)
+    {
+        
+        for (int i = 0; i < TalkDic[TalkSelect].Count; i++)
         {
             //각 대화마다 실행해야 할 내용 여기다가
-            TalkerChange(StoryData.talks[i].Talker);
-            BackgroundChange(StoryData.talks[i].Background);
-            NameTxt.text = StoryData.talks[i].Talker;
-            yield return StartCoroutine(Typing(TalkTxt, StoryData.talks[i].talk));
+            TalkerChange(TalkDic[TalkSelect][i].Talker);
+            BackgroundChange(TalkDic[TalkSelect][i].Background);
+            NameTxt.text = TalkDic[TalkSelect][i].Talker;
+            yield return StartCoroutine(Typing(TalkTxt, TalkDic[TalkSelect][i].talk));
 
-            if (i + 1 == StoryData.talks.Count) continue;
+            if (i + 1 == TalkDic[TalkSelect].Count) continue;
             yield return StartCoroutine(Waiting());
         }
     }
@@ -84,13 +141,43 @@ public class Dialoue : MonoBehaviour
     {
         switch (talker)
         {
-            case "캐릭 1":
-                Character[0].color = new Color(50 / 255f, 50 / 255f, 50 / 255f, 1);
-                Character[0].rectTransform.localScale = new Vector2(0.8f, 0.8f);
-                Character[1].color = new Color(1, 1, 1, 1);
-                Character[1].rectTransform.localScale = new Vector2(1, 1);
+            case "이태연":
+                //Character[0].sprite = 
+                Character[1].color = new Color(50 / 255f, 50 / 255f, 50 / 255f, 1);
+                Character[1].rectTransform.localScale = new Vector2(0.8f, 0.8f);
+                Character[0].color = new Color(1, 1, 1, 1);
+                Character[0].rectTransform.localScale = new Vector2(1, 1);
                 break;
-            case "캐릭 2":
+            case "정서윤":
+                //Character[0].sprite = 
+                Character[1].color = new Color(50 / 255f, 50 / 255f, 50 / 255f, 1);
+                Character[1].rectTransform.localScale = new Vector2(0.8f, 0.8f);
+                Character[0].color = new Color(1, 1, 1, 1);
+                Character[0].rectTransform.localScale = new Vector2(1, 1);
+                break;
+            case "이예린":
+                //Character[0].sprite = 
+                Character[1].color = new Color(50 / 255f, 50 / 255f, 50 / 255f, 1);
+                Character[1].rectTransform.localScale = new Vector2(0.8f, 0.8f);
+                Character[0].color = new Color(1, 1, 1, 1);
+                Character[0].rectTransform.localScale = new Vector2(1, 1);
+                break;
+            case "이예린 집사":
+                //Character[0].sprite = 
+                Character[1].color = new Color(50 / 255f, 50 / 255f, 50 / 255f, 1);
+                Character[1].rectTransform.localScale = new Vector2(0.8f, 0.8f);
+                Character[0].color = new Color(1, 1, 1, 1);
+                Character[0].rectTransform.localScale = new Vector2(1, 1);
+                break;
+            case "송연하":
+                //Character[0].sprite = 
+                Character[1].color = new Color(50 / 255f, 50 / 255f, 50 / 255f, 1);
+                Character[1].rectTransform.localScale = new Vector2(0.8f, 0.8f);
+                Character[0].color = new Color(1, 1, 1, 1);
+                Character[0].rectTransform.localScale = new Vector2(1, 1);
+                break;
+            case "성준아":
+                //Character[0].sprite = 
                 Character[1].color = new Color(50 / 255f, 50 / 255f, 50 / 255f, 1);
                 Character[1].rectTransform.localScale = new Vector2(0.8f, 0.8f);
                 Character[0].color = new Color(1, 1, 1, 1);
