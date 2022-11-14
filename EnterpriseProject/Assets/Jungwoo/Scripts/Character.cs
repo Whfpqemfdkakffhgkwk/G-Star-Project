ce1bbc4454d2d4d8eb32641de6146eff52bc15e9
sketupcrobet(Notebook) <sketupcrobet@naver.com>
2022-11-14 11:34:06 +0900

    임시 캐릭터 추가

--- EnterpriseProject/Assets/Jungwoo/Scripts/Character.cs
+++ EnterpriseProject/Assets/Jungwoo/Scripts/Character.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+	public GameObject InformationPannel;
+	public GameObject CharacterObj;
+	void Start()
+	{
+		StartCoroutine(MoveCharacter());
+	}
 
-    void Update()
-    {
-        
-    }
+	void Update()
+	{
+
+	}
+
+	public void ClickInformation()
+	{
+
+	}
+
+	IEnumerator MoveCharacter()
+	{
+		int Temp = 0;
+		int LR = 0;
+		while (true)
+		{
+			float XLoc = CharacterObj.GetComponent<RectTransform>().anchoredPosition.x;
+			float YLoc = CharacterObj.GetComponent<RectTransform>().anchoredPosition.y;
+			Temp = Random.Range(0, 12);
+			if (Random.Range(0, 2) == 0)
+			{
+				LR = -1;
+			}
+			else
+			{
+				LR = 1;
+			}
+			for (int i = 0; i < Temp; i++)
+			{
+				CharacterObj.GetComponent<RectTransform>().anchoredPosition = new Vector3(XLoc + i * 15 * LR, YLoc, 0);
+				yield return new WaitForSeconds(0.1f);
+			}
+			yield return new WaitForSeconds(2f);
+		}
+	}
 }
