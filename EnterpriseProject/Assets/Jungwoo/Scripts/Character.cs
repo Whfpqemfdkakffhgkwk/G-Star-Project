51473e6d04c4a185d6a5e07cdb567c3e80decef7
sketupcrobet(Notebook) <sketupcrobet@naver.com>
2022-11-15 00:49:02 +0900

    애니메이션 추가, 기타등등

--- EnterpriseProject/Assets/Jungwoo/Scripts/Character.cs
+++ EnterpriseProject/Assets/Jungwoo/Scripts/Character.cs
@@ -27,22 +27,38 @@ public class Character : MonoBehaviour
 		int LR = 0;
 		while (true)
 		{
-			float XLoc = CharacterObj.GetComponent<RectTransform>().anchoredPosition.x;
-			float YLoc = CharacterObj.GetComponent<RectTransform>().anchoredPosition.y;
-			Temp = Random.Range(0, 12);
+			//float XLoc = CharacterObj.GetComponent<RectTransform>().anchoredPosition.x;
+			float XLoc = CharacterObj.transform.localPosition.x;
+			//float YLoc = CharacterObj.GetComponent<RectTransform>().anchoredPosition.y;
+			float YLoc = CharacterObj.transform.localPosition.y;
+			Temp = Random.Range(15, 31);
 			if (Random.Range(0, 2) == 0)
 			{
 				LR = -1;
+				GetComponent<SpriteRenderer>().flipX = true;
 			}
 			else
 			{
 				LR = 1;
+				GetComponent<SpriteRenderer>().flipX = false;
 			}
+			if (CharacterObj.transform.localPosition.x <= -700)
+			{
+				LR = 1;
+				GetComponent<SpriteRenderer>().flipX = false;
+			}
+			else if (CharacterObj.transform.localPosition.x >= 700)
+			{
+				LR = -1;
+				GetComponent<SpriteRenderer>().flipX = true;
+			}
+			GetComponent<Animator>().SetTrigger("Walk");
 			for (int i = 0; i < Temp; i++)
 			{
-				CharacterObj.GetComponent<RectTransform>().anchoredPosition = new Vector3(XLoc + i * 15 * LR, YLoc, 0);
-				yield return new WaitForSeconds(0.1f);
+				CharacterObj.transform.localPosition = new Vector3(XLoc + i * 10 * LR, YLoc, -1);
+				yield return new WaitForSeconds(0.02f);
 			}
+			GetComponent<Animator>().SetTrigger("Idle");
 			yield return new WaitForSeconds(2f);
 		}
 	}
