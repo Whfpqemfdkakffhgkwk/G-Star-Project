53d19d61cc7d6b33c700c46e2591c0d0cc61adb9
qkrgustn <sdh210212@sdh.hs.kr>
2023-01-27 19:47:48 +0900

    웹툰 기능 구현
    웹툰 여는 기능 구현 필요

---
+++ EnterpriseProject/Assets/HyeonSu/Scripts/WebtoonManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebtoonManager : MonoBehaviour
+{
+    Scrollbar VerticalBar;
+
+    [SerializeField]
+    private GameObject[] Webtoons; 
+
+    [SerializeField]
+    private RectTransform Content;
+
+    [SerializeField]
+    private Button FinishBtn; 
+
+    private void Start()
+    {
+        VerticalBar = GetComponent<ScrollRect>().verticalScrollbar;
+        FinishBtn.onClick.AddListener(() =>
+        {
+            gameObject.SetActive(false);
+            FinishBtn.gameObject.SetActive(false);
+            VerticalBar.value = 1;
+        });
+    }
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < Webtoons.Length; i++)
+        {
+            if(Webtoons[i].activeSelf == true)
+            {
+                Content.sizeDelta = new Vector2(1440, 
+                    Webtoons[i].GetComponent<RectTransform>().rect.height);
+
+                return;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        CheckReadAll();
+    }
+
+    void CheckReadAll()
+    {
+        if (VerticalBar.value != 0)
+            return;
+
+        FinishBtn.gameObject.SetActive(true);
+    }
+
+    
+}
